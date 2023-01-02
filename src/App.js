6637a3a7d9dc2aceaf5ebb24b323f8468b677ec3
kishanurankar <kishanurankar@gmail.com>
2023-01-02 18:21:45 +0530

    Final edit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Git from "./components/Git";
 import Main from "./components/Main";
+import Explore from "./Explore";
 
 function App() {
     return(
         <div>
             <Git/>
             <Main/>
+            <Explore/>
         </div>
     )
 };
